d6f6bb30824064fb55707a40d51d25817f0eb026
azieg-z <alysonzieg@students.calarts.edu>
2024-12-11 16:36:26 -0800

    Add files via upload

---
+++ classX.js
@@ -0,0 +1,18 @@
+
+class mySquare {
+  
+    constructor(x, y, size, col)
+    {
+      this.x = x;
+      this.y = y;
+      this.size = size;
+      this.col = col;
+    }
+    
+    display()
+    {
+      square(this.x,this.y,this.size);
+    }
+    
+  }
+  
